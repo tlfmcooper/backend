34f9ea8086360cb2165622a0552ffb3cb38552a8
Ali Kone <tlfmcooper@gmail.com>
2024-03-13 19:10:40 -0400

    modified index.js and vercel.json

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const dataController = require('./dataController');
+const dataController = require('../dataController');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
